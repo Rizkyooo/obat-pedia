fe6c01d68905ae5a7843c73ef67a26754d014ac4
Rizkyooo <ainurrizkyooo@gmail.com>
2024-05-30 16:25:59 +0800

    bugs

--- src/libs/actions.js
+++ src/libs/actions.js
@@ -4,6 +4,19 @@ import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
 
+export const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+  return url
+}
+
+
 export async function login(formData) {
   const supabase = createClient()
 
@@ -54,7 +67,7 @@ export const loginWithGoogle = async () => {
   const { error, data } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo:`https://obat-pedia.vercel.app/auth/callback`,
+      redirectTo:`${getURL()}/auth/callback`,
     },
   })
   if (error) {
