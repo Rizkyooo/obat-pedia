78be8ca78651e35577d0293f88c8b5494ebc1dc9
Rizkyooo <ainurrizkyooo@gmail.com>
2024-06-07 10:48:26 +0800

    add forum

--- src/app/forum-kesehatan/page.jsx
+++ src/app/forum-kesehatan/page.jsx
@@ -1,14 +1,86 @@
 
 'use client'
-import { Chip } from "@nextui-org/react";
-import { Spinner } from "@nextui-org/react";
-import { useState } from "react";
+import { CirclePlus, PlusIcon } from "lucide-react";
+import {Modal, Tooltip, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
+
 
 export default function Forum(){
+    return (
+      <main className="min-h-screen bg-gray-100">
+        <div className="px-4 py-4">
+          <h3 className="text-lg font-semibold">Forum Kesehatan</h3>
+          <div>
+            <Tooltip content="Buat Forum" placement="left-start" color="danger">
+              <PlusIcon
+                className="fixed bottom-24 right-8  bg-slate-50 justify-center items-center flex rounded-full hover:animate-spin  border-2 border-[#EE0037] hover:transition-all transition-all cursor-pointer"
+                color="#EE0037" size={35}
+              ></PlusIcon>
+            </Tooltip>
+          </div>
+        </div>
+      </main>
+    );
+}
 
+
+
+
+
+export function ModalAddForum(){
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
     return(
-        <main className="min-h-screen flex justify-center items-center">
-                <Chip className=" animate-bounce" color="danger" size="lg">Coming Soon</Chip>
-        </main>
+        <>
+        <Button onPress={onOpen} color="primary">Open Modal</Button>
+      <Modal 
+      isDismissable={false}
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        placement="center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalBody>
+                <Input
+                  autoFocus
+
+                  label="Email"
+                  placeholder="Enter your email"
+                  variant="bordered"
+                />
+                <Input
+
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                />
+                <div className="flex py-2 px-1 justify-between">
+                  <Checkbox
+                    classNames={{
+                      label: "text-small",
+                    }}
+                  >
+                    Remember me
+                  </Checkbox>
+                  <Link color="primary" href="#" size="sm">
+                    Forgot password?
+                  </Link>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+        </>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,134 +15,134 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { logOut } from "@/libs/actions";
 import { UserIcon } from "lucide-react";
-  export default function Header(props) {
-    const {user, name, avatar_url } = props
-    const handleLogout = () => {
-      logOut()
-    }
-    const header = ["/apoteker", "/admin", "/login", "/signup"];
-   
+export default function Header(props) {
+  const { user, name, avatar_url } = props;
+  const handleLogout = () => {
+    logOut();
+  };
+  const header = ["/apoteker", "/admin", "/login", "/signup"];
+
   const pathName = usePathname();
-  const showHeader = header.some(path => pathName.includes(path));
+  const showHeader = header.some((path) => pathName.includes(path));
   return (
     <>
-    {!showHeader && 
-    <Navbar isBlurred={false} maxWidth="xl" isBordered>
-    <NavbarBrand>
-      <p className="text-[#EE0037] font-bold text-inherit">ObatPedia</p>
-    </NavbarBrand>
-    <NavbarContent className="hidden sm:flex gap-4" justify="center">
-      <NavbarItem isActive={pathName === "/"}>
-        <Link
-          className={`${
-            pathName === "/" ? "text-[#EE0037]" : " "
-          } hover:scale-[1.13] hover:text-[#EE0037] hover:duration-100 hover:transition-transform transition-transform`}
-          color="foreground"
-          href="/"
-        >
-          Home
-        </Link>
-      </NavbarItem>
-      <NavbarItem isActive={pathName.includes("/obat-a-z")}>
-        <Link
-          className={`${
-            pathName.includes("/obat-a-z") ? "text-[#EE0037]" : " "
-          } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
-          color="foreground"
-          href="/obat-a-z"
-        >
-          Obat A-Z
-        </Link>
-      </NavbarItem>
-      <NavbarItem isActive={pathName === "/tanya-apoteker"}>
-        <Link
-          className={`${
-            pathName === "/tanya-apoteker" ? "text-[#EE0037]" : " "
-          } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
-          color="foreground"
-          href="/tanya-apoteker"
-        >
-          Tanya Apoteker
-        </Link>
-      </NavbarItem>
-      <NavbarItem isActive={pathName === "/forum-kesehatan"}>
-        <Link
-          className={`${
-            pathName === "/forum-kesehatan" ? "text-[#EE0037]" : " "
-          } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
-          color="foreground"
-          href="/forum-kesehatan"
-        >
-          Forum Kesehatan
-        </Link>
-      </NavbarItem>
-      <NavbarItem isActive={pathName === "/artikel"}>
-        <Link
-          className={`${
-            pathName === "/artikel" ? "text-[#EE0037]" : " "
-          } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
-          color="foreground"
-          href="/artikel"
-        >
-          Artikel
-        </Link>
-      </NavbarItem>
-    </NavbarContent>
-    <NavbarContent justify="end">
-      <NavbarItem>
-        {user ? (
-          <Dropdown>
-            <DropdownTrigger>
-              <div className="flex items-center justify-center  gap-3">
-                <Avatar
-                  className="transition-transform"
-                  as="button"
-                  size="sm"
-                  isBordered
-                  icon={<UserIcon />}
-                  src={user.avatar_url ||"https://i.pinimg.com/736x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg"}
-                />
-                <div className="sm:flex flex-col hidden">
-                  <p className="text-xs font-medium">
-                    {name}
-                  </p>
-                  <p className="text-[0.7rem]">{user.email}</p>
-                </div>
-              </div>
-            </DropdownTrigger>
-            <DropdownMenu aria-label="a">
-              <DropdownItem className=" pl-3" href="/profil" key="profil">
-                Profile
-              </DropdownItem>
-              <DropdownItem href="/login" key="logout">
+      {!showHeader && (
+        <Navbar isBlurred={false} maxWidth="xl" isBordered>
+          <NavbarBrand>
+            <p className="text-[#EE0037] font-bold text-inherit">ObatPedia</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem isActive={pathName === "/"}>
+              <Link
+                className={`${
+                  pathName === "/" ? "text-[#EE0037]" : " "
+                } hover:scale-[1.13] hover:text-[#EE0037] hover:duration-100 hover:transition-transform transition-transform`}
+                color="foreground"
+                href="/"
+              >
+                Home
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive={pathName.includes("/obat-a-z")}>
+              <Link
+                className={`${
+                  pathName.includes("/obat-a-z") ? "text-[#EE0037]" : " "
+                } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
+                color="foreground"
+                href="/obat-a-z"
+              >
+                Obat A-Z
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive={pathName === "/tanya-apoteker"}>
+              <Link
+                className={`${
+                  pathName === "/tanya-apoteker" ? "text-[#EE0037]" : " "
+                } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
+                color="foreground"
+                href="/tanya-apoteker"
+              >
+                Tanya Apoteker
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive={pathName === "/forum-kesehatan"}>
+              <Link
+                className={`${
+                  pathName === "/forum-kesehatan" ? "text-[#EE0037]" : " "
+                } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
+                color="foreground"
+                href="/forum-kesehatan"
+              >
+                Forum Kesehatan
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive={pathName === "/artikel"}>
+              <Link
+                className={`${
+                  pathName === "/artikel" ? "text-[#EE0037]" : " "
+                } hover:text-[#EE0037] hover:scale-[1.13] hover:duration-100 hover:transition-transform transition-transform`}
+                color="foreground"
+                href="/artikel"
+              >
+                Artikel
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem>
+              {user ? (
+                <Dropdown>
+                  <DropdownTrigger>
+                    <div className="flex items-center justify-center  gap-3">
+                      <Avatar
+                        className="transition-transform"
+                        as="button"
+                        size="sm"
+                        isBordered
+                        icon={<UserIcon />}
+                        src={
+                          user.avatar_url ||
+                          "https://i.pinimg.com/736x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg"
+                        }
+                      />
+                      <div className="sm:flex flex-col hidden">
+                        <p className="text-xs font-medium">{name}</p>
+                        <p className="text-[0.7rem]">{user.email}</p>
+                      </div>
+                    </div>
+                  </DropdownTrigger>
+                  <DropdownMenu aria-label="a">
+                    <DropdownItem className=" pl-3" key="profil">
+                      <Link href="/profil"> Profile</Link>
+                    </DropdownItem>
+                    <DropdownItem href="/login" key="logout">
+                      <Button
+                        onClick={handleLogout}
+                        size="sm"
+                        className="bg-[#EE0037] text-white"
+                        variant="flat"
+                      >
+                        Logout
+                      </Button>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              ) : (
                 <Button
-                onClick={handleLogout}
                   size="sm"
                   className="bg-[#EE0037] text-white"
+                  as={Link}
+                  color=""
+                  href="/login"
                   variant="flat"
                 >
-                  Logout
+                  Login
                 </Button>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        ) : (
-          <Button
-            size="sm"
-            className="bg-[#EE0037] text-white"
-            as={Link}
-            color=""
-            href="/login"
-            variant="flat"
-          >
-            Login
-          </Button>
-        )}
-      </NavbarItem>
-    </NavbarContent>
-  </Navbar>
-    }
-    
+              )}
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      )}
     </>
   );
 }
