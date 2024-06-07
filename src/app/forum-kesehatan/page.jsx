c14b9915bb71fb6e204c531f8d998e3e1f81d92c
Rizkyooo <ainurrizkyooo@gmail.com>
2024-06-07 15:23:31 +0800

    update forum

--- src/app/forum-kesehatan/page.jsx
+++ src/app/forum-kesehatan/page.jsx
@@ -1,36 +1,63 @@
 
 'use client'
-import {Modal, Select, SelectItem, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
+import {Modal, Select, SelectItem, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Textarea, Input, RadioGroup, Radio} from "@nextui-org/react";
 import ForumItem from "@/components/forumItem";
 
 
 export default function Forum(){
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
     return (
-      <main className="min-h-screen bg-gray-100">
-        <div className="px-4 py-4">
-          <h3 className="text-lg font-semibold pl-1">Forum Kesehatan</h3>
-          <div className="flex justify-center items-center gap-2 mt-4">
+      <main className="min-h-screen bg-gray-100 mx-auto sm:px-6 sm:relative">
+        <h3 className="text-lg font-semibold p-4">Forum Kesehatan</h3>
+        <div className="px-4 sm:flex sm:gap-4">
+          <div className="flex justify-center items-center gap-2 sm:hidden mb-6">
             <Select
-             size="sm" radius="md" color={"#EE0037"} variant="bordered" label="Topik">
-                <SelectItem>
-                    Obat
-                </SelectItem>
-                <SelectItem>
-                    Kesehatan
-                </SelectItem>
+              className="shadow-xs"
+              size="sm"
+              radius="md"
+              color={"#EE0037"}
+              variant="bordered"
+              label="Topik"
+            >
+              <SelectItem>Obat</SelectItem>
+              <SelectItem>Kesehatan</SelectItem>
             </Select>
-            <Button color='danger'>Buat</Button>
+            <Button onPress={onOpen} className="shadow-sm" color="danger">
+              Buat
+            </Button>
           </div>
 
-          <div className="pl-1 flex flex-col justify-center items-start mt-6 gap-2">
-            <ForumItem/>
-            <ForumItem/>
-            <ForumItem/>
-            <ForumItem/>
-            <ForumItem/>
+          <div className="hidden sm:block sm:w-2/6">
+            <div className="sticky top-20 z-10 flex flex-col justify-start px-6 py-6 bg-white rounded-lg shadow-sm ">
+              <Button onPress={onOpen} fullWidth color="danger">
+                Buat Diskusi Baru
+              </Button>
+              <RadioGroup className="mt-4">
+                <Radio value="buenos-aires">Buenos Aires</Radio>
+                <Radio value="sydney">Sydney</Radio>
+                <Radio value="san-francisco">San Francisco</Radio>
+                <Radio value="london">London</Radio>
+                <Radio value="tokyo">Tokyo</Radio>
+              </RadioGroup>
+            </div>
+          </div>
 
+          <div className="pl-1 flex flex-col justify-center items-center gap-2 sm:w-4/6">
+            <ForumItem />
+            <ForumItem />
+            <ForumItem />
+            <ForumItem />
+            <ForumItem />
+            <Button size="sm" className="mt-4" color="danger" variant="ghost">
+              Load More
+            </Button>
           </div>
         </div>
+        <ModalAddForum
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onOpenChange={onOpenChange}
+        />
       </main>
     );
 }
@@ -39,11 +66,9 @@ export default function Forum(){
 
 
 
-export function ModalAddForum(){
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+export function ModalAddForum({isOpen, onOpenChange, onOpen}){
     return(
         <>
-        <Button onPress={onOpen} color="primary">Open Modal</Button>
       <Modal 
       isDismissable={false}
         isOpen={isOpen} 
@@ -53,41 +78,46 @@ export function ModalAddForum(){
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">Buat Diskusi Baru</ModalHeader>
               <ModalBody>
                 <Input
+                isRequired
+                label="Judul"
+                labelPlacement="outside"
                   autoFocus
-
-                  label="Email"
-                  placeholder="Enter your email"
+                  placeholder="Tambahkan Judul"
                   variant="bordered"
                 />
-                <Input
-
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
-                  variant="bordered"
-                />
-                <div className="flex py-2 px-1 justify-between">
-                  <Checkbox
-                    classNames={{
-                      label: "text-small",
-                    }}
-                  >
-                    Remember me
-                  </Checkbox>
-                  <Link color="primary" href="#" size="sm">
-                    Forgot password?
-                  </Link>
-                </div>
+                <Textarea 
+                fullWidth
+                variant="bordered"
+      isRequired={true}
+      label="Deskripsi"
+      labelPlacement="outside"
+      placeholder="Isi Deskripsi"
+    />
+                <Select className="shadow-xs"
+             size="md" radius="md" color={"#EE0037"} variant="bordered" placeholder="Pilih Topik" labelPlacement="outside">
+                <SelectItem>
+                    Obat
+                </SelectItem>
+                <SelectItem>
+                    Kesehatan
+                </SelectItem>
+                <SelectItem>
+                    Kesehatan
+                </SelectItem>
+                <SelectItem>
+                    Kesehatan
+                </SelectItem>
+            </Select>
               </ModalBody>
               <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
+                <Button  variant="flat" onPress={onClose}>
+                  Batal
                 </Button>
-                <Button color="primary" onPress={onClose}>
-                  Sign in
+                <Button color="danger" onPress={onClose}>
+                  Submit
                 </Button>
               </ModalFooter>
             </>
--- src/components/forumItem.jsx
+++ src/components/forumItem.jsx
@@ -1,7 +1,7 @@
 import { Avatar } from "@nextui-org/react"
 import Link from "next/link";
 import { MessageCircleMore } from "lucide-react";
-export default function ForumItem() {
+export default function ForumItem({image, judul, deskripsi, penulis, }) {
     const truncateText = (text, maxLength) => {
         if (text.length > maxLength) {
           return text.slice(0, maxLength) + " ...";
@@ -9,19 +9,20 @@ export default function ForumItem() {
         return text;
     }
   return (
-    <div className="flex p-2 gap-4 bg-white rounded-lg w-full justify-center items-center">
-        <Avatar className="w-24 h-24" src="https://i.pinimg.com/736x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg"/>
-        <div className="my-2">
-            <p className="text-sm font-semibold mb-1">{truncateText("Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.", 40)}</p>
+    <Link href={"#"} className="flex p-2 gap-4 bg-white rounded-lg w-full justify-start items-center shadow-sm">
+        <Avatar  className=" w-14 h-auto sm:w-20" src="/images/obat-icon.svg"/>
+        <div className="w-full py-4">
+            <p className="text-sm sm:text-lg font-semibold mb-1 sm:mb-0">{truncateText("Lorem ipsum dolor sit amet consectetur adipisicingsss elit. Quisquam, quidem.", 50)}</p>
+            <p className="sm:block text-md mb-1 hidden">{truncateText("Lorem ipsum dolor sit amet consectetur adipisicingsss elit. Quisquam, quidem. r adipisicingsss elit. Quisquam, quidem. gsss elit. Quisquam, quidem. r adipisicingsss elit. Quisquam, quidem.  Quisquam, quidem. r adipisicingsss elit. Quisquam, quidem.", 200)}</p>
             <p className="text-xs mb-4">jhon Doe</p>
-            <div className="flex justify-between">
-                <div className="flex gap-1">
-                <MessageCircleMore className="text-slate-600" size={16}/>
-                <p className="text-xs">1,000</p>
+            <div className="flex justify-between ">
+                <div className="flex gap-1 ">
+                <MessageCircleMore className="text-slate-600 opacity-75" size={15}/>
+                <p className="text-xs opacity-75">1,000</p>
                 </div>
-            <Link  href={"#"} className="text-xs text-[#EE0037] px-2">Selengkapnya...</Link>
+            <p  href={"#"} className="text-[0.6rem] font-semibold opacity-75 px-2">1 menit yg lalu</p>
             </div>
         </div>
-    </div>
+    </Link>
   )
 }
