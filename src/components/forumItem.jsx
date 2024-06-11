e1bb8f680bb9507c674665f85dddbc1d4904a8a2
Rizkyooo <ainurrizkyooo@gmail.com>
2024-06-11 15:44:32 +0800

    update forum ui

--- src/components/forumItem.jsx
+++ src/components/forumItem.jsx
@@ -1,40 +1,73 @@
-import { Avatar } from "@nextui-org/react"
+import { Avatar, User } from "@nextui-org/react";
 import Link from "next/link";
 import { MessageCircleMore, UserCircle } from "lucide-react";
-import { formatDistanceToNow } from 'date-fns';
-import { id as localeID } from 'date-fns/locale';
-export default function ForumItem({id,image, judul, deskripsi, penulis, jml_komentar, date}) {
+import { formatDistanceToNow } from "date-fns";
+import { id as localeID } from "date-fns/locale";
+export default function ForumItem({
+  id,
+  image,
+  judul,
+  deskripsi,
+  penulis,
+  jml_komentar,
+  date,
+}) {
   const TimeAgo = ({ date }) => {
     return (
-      <span>{formatDistanceToNow(new Date(date), { addSuffix: true, locale: localeID })}</span>
+      <span>
+        {formatDistanceToNow(new Date(date), {
+          addSuffix: true,
+          locale: localeID,
+        })}
+      </span>
     );
   };
-  
-    const truncateText = (text, maxLength) => {
-        if (text?.length > maxLength) {
-          return text?.slice(0, maxLength) + " ...";
-        }
-        return text;
+
+  const truncateText = (text, maxLength) => {
+    if (text?.length > maxLength) {
+      return text?.slice(0, maxLength) + " ...";
     }
+    return text;
+  };
 
-    const encodedTitle = encodeURIComponent(judul).toLocaleLowerCase();
+  const encodedTitle = encodeURIComponent(judul).toLocaleLowerCase();
   return (
-    
-    <Link href={`/forum-kesehatan/${encodedTitle}?id=${id}`} className="flex p-2 gap-4 bg-white rounded-lg justify-start items-center sm:items-start shadow-sm w-full">
-        <Avatar  icon = {<UserCircle/>}  className=" h-8 sm:h-10  sm:mt-6  " src={image}/>
-
-        <div className="w-full py-4">
-            <p className="text-sm sm:text-lg font-semibold mb-1 sm:mb-0">{truncateText(judul, 50)}</p>
-            <p className="sm:block text-sm mb-1 hidden">{truncateText(deskripsi, 200)}</p>
-            <p className="text-xs mb-4">{penulis}</p>
-            <div className="flex justify-between ">
-                <div className="flex gap-1 ">
-                <MessageCircleMore className="text-slate-600 opacity-75" size={15}/>
-                <p className="text-xs opacity-75">{jml_komentar}</p>
-                </div>
-            <p  href={"#"} className="text-[0.65rem]  font-semibold opacity-55 px-2"><TimeAgo date={date} /></p>
-            </div>
+    <Link
+      href={`/forum-kesehatan/${encodedTitle}?id=${id}`}
+      className="flex p-4 flex-col gap-2 bg-white rounded-lg justify-start items-start sm:items-start shadow-sm w-full"
+    >
+      <User
+        name={penulis}
+        description="pengguna"
+        avatarProps={{
+          src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+          size: "sm",
+        }}
+      />
+      <div className="w-full flex flex-col gap-1">
+        <p className="text-sm pl-10 sm:text-lg font-semibold sm:mb-0">
+          {truncateText(judul, 50)}
+        </p>
+        <p className="sm:block pl-10 text-sm mb-1 hidden">
+          {truncateText(deskripsi, 200)}
+        </p>
+        {/* <p className="text-xs mb-4">{penulis}</p> */}
+        <div className="flex justify-between mt-4">
+          <div className="flex gap-1 ">
+            <MessageCircleMore
+              className="text-slate-600 opacity-75"
+              size={15}
+            />
+            <p className="text-xs opacity-75">{jml_komentar}</p>
+          </div>
+          <p
+            href={"#"}
+            className="text-[0.65rem]  font-semibold opacity-55 px-2"
+          >
+            <TimeAgo date={date} />
+          </p>
         </div>
+      </div>
     </Link>
-  )
+  );
 }
