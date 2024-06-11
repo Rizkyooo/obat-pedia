c53237f5543e7bb8237f669bf717f63a63fa9fd9
Rizkyooo <ainurrizkyooo@gmail.com>
2024-06-11 20:31:18 +0800

    fix bugs

--- src/app/forum-kesehatan/page.jsx
+++ src/app/forum-kesehatan/page.jsx
@@ -1,64 +1,60 @@
-'use client';
 import ForumItem from "@/components/forumItem";
 import ForumKategori from "@/components/forumKategori";
 import { getUser } from "@/libs/actions";
 import { createClient } from "@/utils/supabase/client";
 import { Button } from "@nextui-org/react";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Forum(){
-  const [forum, setForum] = useState([]);
-  const [loadMore , setLoadMore] = useState(14);
-  const [isLoading, setIsLoading] = useState(false);
-  const [user, setUser] = useState(null)
+export default async function Forum(){
+  const user  = await getUser();
+  // const [forum, setForum] = useState([]);
+  // const [loadMore , setLoadMore] = useState(14);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [user, setUser] = useState(null)
 
-  async function fetchForum(limit) {
-    setIsLoading(true);
-    const supabase = createClient();
-    try {
-      const { data, error } = await supabase
-        .from("diskusi")
-        .select("*")
-        .order("created_at", { ascending: false })
-        .range(0, limit);
-      if (error) {
-        console.log(error);
-      }
-      if(data){
-        setForum(data);
-        setIsLoading(false);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // async function fetchForum(limit) {
+  //   setIsLoading(true);
+  //   const supabase = createClient();
+  //   try {
+  //     const { data, error } = await supabase
+  //       .from("diskusi")
+  //       .select("*")
+  //       .order("created_at", { ascending: false })
+  //       .range(0, limit);
+  //     if (error) {
+  //       console.log(error);
+  //     }
+  //     if(data){
+  //       setForum(data);
+  //       setIsLoading(false);
+  //       return data
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  useEffect(() => {
-    async function getUsers(){
-      const user = await getUser()
-      setUser(user)
-    }
-    getUsers();
-    fetchForum(loadMore);
+  // useEffect(() => {
+  //   async function getUsers(){
+  //     const user = await getUser()
+  //     setUser(user)
+  //   }
+  //   getUsers();
+  //   fetchForum(loadMore);
     
-  }, [])
+  // }, [])
 
-  const handleMore = () => {
-    setLoadMore(loadMore + 14);
-    fetchForum(loadMore+14);
-  };
+  // const handleMore = () => {
+  //   setLoadMore(loadMore + 14);
+  //   fetchForum(loadMore+14);
+  // };
 
     return (
     <main className="">
-      <div className="min-h-screen flex gap-2">
+      <div className="min-h-screen sm:flex gap-2">
       <ForumKategori checkUser={user}/>
           <div className=" flex flex-col justify-center items-center gap-2 sm:w-4/6">
-            {forum?.map((forum) => (
-            <ForumItem id={forum?.id} key={forum?.id} date={forum?.created_at} jml_komentar={forum?.jml_komentar} penulis={forum?.penulis} judul={forum?.judul} deskripsi={forum?.deskripsi} image={"/images/obat-icon.svg"}/>
-              ))}
-            <Button isLoading={isLoading} onPress={handleMore} size="sm" className="mt-4" color="danger" variant="ghost">
-              Load More
-            </Button>
+            <ForumItem />
           </div>
       </div>
     </main>
--- src/components/forumItem.jsx
+++ src/components/forumItem.jsx
@@ -1,17 +1,41 @@
-import { Avatar, User } from "@nextui-org/react";
+"use client";
+import { Button, User } from "@nextui-org/react";
 import Link from "next/link";
-import { MessageCircleMore, UserCircle } from "lucide-react";
+import { MessageCircleMore } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 import { id as localeID } from "date-fns/locale";
-export default function ForumItem({
-  id,
-  image,
-  judul,
-  deskripsi,
-  penulis,
-  jml_komentar,
-  date,
-}) {
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+export default function ForumItem() {
+  const [forum, setForum] = useState([]);
+  const [loadMore, setLoadMore] = useState(14);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchForum(limit) {
+    setIsLoading(true);
+    const supabase = createClient();
+    try {
+      const { data, error } = await supabase
+        .from("diskusi")
+        .select("*")
+        .order("created_at", { ascending: false })
+        .range(0, limit);
+      if (error) {
+        console.log(error);
+      }
+      if (data) {
+        setForum(data);
+        setIsLoading(false);
+        return data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchForum(loadMore);
+  }, []);
   const TimeAgo = ({ date }) => {
     return (
       <span>
@@ -23,6 +47,11 @@ export default function ForumItem({
     );
   };
 
+  const handleMore = () => {
+    setLoadMore(loadMore + 14);
+    fetchForum(loadMore + 14);
+  };
+
   const truncateText = (text, maxLength) => {
     if (text?.length > maxLength) {
       return text?.slice(0, maxLength) + " ...";
@@ -30,44 +59,58 @@ export default function ForumItem({
     return text;
   };
 
-  const encodedTitle = encodeURIComponent(judul).toLocaleLowerCase();
+  const encodedTitle = encodeURIComponent(forum?.judul).toLocaleLowerCase();
   return (
-    <Link
-      href={`/forum-kesehatan/${encodedTitle}?id=${id}`}
-      className="flex p-4 flex-col gap-2 bg-white rounded-lg justify-start items-start sm:items-start shadow-sm w-full"
-    >
-      <User
-        name={penulis}
-        description="pengguna"
-        avatarProps={{
-          src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-          size: "sm",
-        }}
-      />
-      <div className="w-full flex flex-col gap-1">
-        <p className="text-sm  sm:text-lg font-semibold sm:mb-0">
-          {truncateText(judul, 50)}
-        </p>
-        <p className="sm:block text-sm mb-1 hidden">
-          {truncateText(deskripsi, 200)}
-        </p>
-        {/* <p className="text-xs mb-4">{penulis}</p> */}
-        <div className="flex justify-between mt-4">
-          <div className="flex gap-1 ">
-            <MessageCircleMore
-              className="text-slate-600 opacity-75"
-              size={15}
-            />
-            <p className="text-xs opacity-75">{jml_komentar}</p>
-          </div>
-          <p
-            href={"#"}
-            className="text-[0.65rem]  font-semibold opacity-55 px-2"
-          >
-            <TimeAgo date={date} />
+    <>
+    {forum?.map((forum) => (  
+      <Link
+        href={`/forum-kesehatan/${encodedTitle}?id=${forum?.id}`}
+        className="flex p-4 flex-col gap-2 bg-white rounded-lg justify-start items-start sm:items-start shadow-sm w-full"
+      >
+        <User
+          name={forum?.penulis}
+          description="pengguna"
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+            size: "sm",
+          }}
+        />
+        <div className="w-full flex flex-col gap-1">
+          <p className="text-sm  sm:text-lg font-semibold sm:mb-0">
+            {truncateText(forum?.judul, 50)}
           </p>
+          <p className="sm:block text-sm mb-1 hidden">
+            {truncateText(forum?.deskripsi, 200)}
+          </p>
+          {/* <p className="text-xs mb-4">{penulis}</p> */}
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-1 ">
+              <MessageCircleMore
+                className="text-slate-600 opacity-75"
+                size={15}
+              />
+              <p className="text-xs opacity-75">{forum?.jml_komentar}</p>
+            </div>
+            <p
+              href={"#"}
+              className="text-[0.65rem]  font-semibold opacity-55 px-2"
+            >
+              <TimeAgo date={forum?.created_at} />
+            </p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    ))}
+      <Button
+        isLoading={isLoading}
+        onPress={handleMore}
+        size="sm"
+        className="mt-4"
+        color="danger"
+        variant="ghost"
+      >
+        Load More
+      </Button>
+    </>
   );
 }
