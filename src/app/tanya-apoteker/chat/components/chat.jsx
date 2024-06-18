979268e7a4abb0a87480499d2c6e3bdb92fb6e14
Rizkyooo <ainurrizkyooo@gmail.com>
2024-06-18 16:30:32 +0800

    test

--- src/app/tanya-apoteker/chat/components/chat.jsx
+++ src/app/tanya-apoteker/chat/components/chat.jsx
@@ -1,8 +1,14 @@
+'use client';
 import { createClient } from "@/utils/supabase/client";
 import { Button, Input, User } from "@nextui-org/react";
 import { ArrowLeft, Send } from "lucide-react";
 import Link from "next/link";
-export default async function Chat({ id }) {
+import { useEffect, useRef, useState } from "react";
+
+export default function Chat({ id }) {
+  const [apoteker, setApoteker] = useState(null);
+  const scrollRef = useRef(null);
+
   async function getApoteker(id) {
     if (!id) return;
     const supabase = createClient();
@@ -14,14 +20,21 @@ export default async function Chat({ id }) {
     if (error) {
       throw new Error(error.message);
     }
-    return data;
+    setApoteker(data);
   }
 
-  const apoteker = await getApoteker(id);
-  console.log(apoteker);
+  useEffect(() => {
+    getApoteker(id);
+  }, [id]);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [apoteker]);
+
   return (
     <div className="bg-gray-100">
-
       <div className="px-4 py-2 bg-gray-300 flex gap-1 items-center">
         <div className="flex gap-1 items-center">
           <Link href={`/tanya-apoteker/chat`}>
@@ -40,64 +53,55 @@ export default async function Chat({ id }) {
       </div>
       
       <div className="mb-14">
-
-      <div className="overflow-y-scroll  h-[calc(100vh-170px)] sm:h-[calc(100vh-200px)] bg-gray-100 flex justify-center">
-        <div className="w-full flex flex-col items-center pt-10">
-          <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
-          <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
-          <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
-            <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
+        <div ref={scrollRef} className="overflow-y-scroll h-[calc(100vh-170px)] sm:h-[calc(100vh-200px)] bg-gray-100 flex justify-center">
+          <div className="w-full flex flex-col items-center pt-10">
+            <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
+              <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
+              <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
+              <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col">
+              <p className="text-sm pt-1">okey mantap nih</p>
+              <p className="text-[0.55rem] self-end">1.48 PM</p>
+              <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
+              <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
+              <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
+              <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col">
+              <p className="text-sm pt-1">okey mantap nih</p>
+              <p className="text-[0.55rem] self-end">1.48 PM</p>
+              <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
+              <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
+              <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
+              <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col">
+              <p className="text-sm pt-1">okey mantap nih</p>
+              <p className="text-[0.55rem] self-end">1.48 PM</p>
+              <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
+              <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
+              <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
+              <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
+            </div>
+            <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col">
+              <p className="text-sm pt-1">okey mantap nih</p>
+              <p className="text-[0.55rem] self-end">1.48 PM</p>
+              <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
+            </div>
           </div>
-
-          <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white  px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col ">
-            <p className="text-sm pt-1">okey mantap nih</p>
-            <p className="text-[0.55rem] self-end">1.48 PM</p>
-            <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
-          </div>
-
-          <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
-          <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
-          <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
-            <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
-          </div>
-
-          <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white  px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col ">
-            <p className="text-sm pt-1">okey mantap nih</p>
-            <p className="text-[0.55rem] self-end">1.48 PM</p>
-            <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
-          </div>
-
-          <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
-          <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
-          <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
-            <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
-          </div>
-
-          <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white  px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col ">
-            <p className="text-sm pt-1">okey mantap nih</p>
-            <p className="text-[0.55rem] self-end">1.48 PM</p>
-            <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
-          </div>
-
-          <div className="relative self-start text-sm max-w-[50%] flex flex-col bg-white text-black px-2 py-1 rounded-lg shadow-md mb-4 ml-4">
-          <p className="text-sm pt-1">Hai saya Apt. {apoteker?.nama} Apakah ada yang bisa kami bantu?</p>
-          <p className="text-[0.55rem] px-2 self-end">1.48 PM</p>
-            <div className="absolute top-0 left-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-r-[16px] border-r-white border-b-[16px] border-b-transparent"></div>
-          </div>
-
-          <div className="relative max-w-[50%] self-end text-sm bg-[#EE0037] text-white  px-2 py-1 rounded-lg shadow-md mb-4 mr-4 flex flex-col ">
-            <p className="text-sm pt-1">okey mantap nih</p>
-            <p className="text-[0.55rem] self-end">1.48 PM</p>
-            <div className="absolute top-0 right-[-8px] w-0 h-0 border-t-[16px] border-t-transparent border-l-[16px] border-l-[#EE0037] border-b-[16px] border-b-transparent"></div>
-          </div>
-
         </div>
       </div>
-      </div>
 
-      <div className=" justify-center items-center w-full fixed sm:sticky flex bottom-0 sm:bottom-2 gap-1 px-2 bg-gray-100">
+      <div className="justify-center items-center w-full fixed sm:sticky flex bottom-0 sm:bottom-2 gap-1 px-2 bg-gray-100">
         <Input
-        className="bg-white rounded-full"
+          className="bg-white rounded-full"
           type="text"
           variant="bordered"
           placeholder="masukkan pesan"
