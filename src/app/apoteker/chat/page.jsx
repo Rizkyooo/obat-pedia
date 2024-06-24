27c981c8714af0a9806f142121fd769f55270e37
Rizkyooo <ainurrizkyooo@gmail.com>
2024-06-24 08:49:05 +0800

    testtt

--- src/app/apoteker/chat/page.jsx
+++ src/app/apoteker/chat/page.jsx
@@ -2,7 +2,7 @@
 export default function page() {
   return (
     <div className="hidden  sm:flex w-3/4 max-h-screen gap-1">
-      <div className="w-full shadow-md bg-white">chat</div>
+      <div className="w-full shadow-md bg-white flex justify-center items-center font-semibold">Pilih Salah Satu Chat</div>
     </div>
   )
 }
