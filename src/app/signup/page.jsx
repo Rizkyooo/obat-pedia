83f953d246a781166ced1d1384108ca6158a43ae
Rizkyooo <ainurrizkyooo@gmail.com>
2024-07-08 19:19:18 +0800

    update signup

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -39,7 +39,7 @@ export default function SignupPage() {
       options: {
         data: {
           name: name,
-          role: 'apoteker', 
+          role: 'pengguna', 
         },
       }
     }
