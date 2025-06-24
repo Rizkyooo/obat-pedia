ab04025e0d31d6ebf57587a1a41d3c727fd831e1
Rizkyooo <ainurrizkyooo@gmail.com>
2025-06-24 22:00:28 +0800

    ta

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      outputFileTracingIncludes: {
-        '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.node'],
-      },
-    },
-    // Atau gunakan ini untuk versi Next.js yang lebih lama
-    outputFileTracing: true,
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
+
+export default nextConfig;
