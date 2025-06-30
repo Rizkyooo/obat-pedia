6271c0bf990a16b5df472fdef19714fe4bec9b25
Rizkyooo <ainurrizkyooo@gmail.com>
2025-06-30 20:03:26 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ensure prisma engine files are copied to the build output
-      config.externals.push("@prisma/client");
-    }
-    return config;
-  },
+    devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
