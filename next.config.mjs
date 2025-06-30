5c0292006481fbfdc8d560e64685c212eb61e5d3
Rizkyooo <ainurrizkyooo@gmail.com>
2025-06-30 19:54:44 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ensure prisma engine files are copied to the build output
+      config.externals.push("@prisma/client");
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
